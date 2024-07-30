026411fe5bda69b1b61f64a50a4debb42b79c9e4
Simon Van Casteren <simon.van.casteren@gmail.com>
2024-07-30 09:22:12 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {self, nixpkgs, flake-utils} :
+    flake-utils.lib.eachSystem ["x86_64-linux"]
+      (system:
+        let
+          pkgs = import nixpkgs { system = system; };
+          # allPackages = import ./default.nix {
+          #   pkgs = pkgs;
+          #   urweb = urweb.defaultPackage.${system};
+          #   urweb-curl = urweb-curl;
+          # };
+        in
+          {
+            devShell = pkgs.mkShell {
+              name = "school-env";
+              buildInputs = [
+                             pkgs.nodejs_21
+                            ];
+              shellHook = ''
+              '';
+            };
+          }
+      );
+}
