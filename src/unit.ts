2a8de72fc8024db7645ecc32c4cd692d1dcaaeab
Simon Van Casteren <simon.van.casteren@gmail.com>
2024-09-08 00:35:14 +0200

    remove prints

--- src/unit.ts
+++ src/unit.ts
@@ -324,14 +324,14 @@ export function translateUnit(
 
 export function calcRaidGroupIndex(sources: sources): null | number {
   const playerName = sources.player.name.get();
-  print(`player: ${playerName}`);
+  // print(`player: ${playerName}`);
   for (let i of [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]) {
     const [name, rank, subgroup] = GetRaidRosterInfo(i);
-    if (name) {
-      print(`${i}. raid name: ${name}, subgroup ${subgroup}`);
-    }
+    // if (name) {
+    //   print(`${i}. raid name: ${name}, subgroup ${subgroup}`);
+    // }
     if (name && name === playerName) {
-      print(`Found: subgroup ${subgroup} - 1`);
+      // print(`Found: subgroup ${subgroup} - 1`);
       return subgroup - 1;
     }
   }
